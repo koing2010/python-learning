911b483d9b4d4b70b27f88483a3e415a499e665f
koing2010 <koing2010@163.com>
2017-07-05 20:46:12 +0800

    实现部分通信

--- serial_test.py
+++ serial_test.py
@@ -1,13 +1,38 @@
 import serial
 import time
 import threading
+import struct
+txmsg = ('\x5B\xB5\x19\x00\x78\xB8\x01\x08\x23\xB4\xEC\x11\x00\x4B\x12\x00\x01\x06\x00\x00')
+# define every command of
+Send_Lenth = 0x0001
+Send_SN = 1
+TyHeader = 0xB8
+ProtocolType= 0x01
+IEEE64 = (b'\x08\x7A\xB3\x6A\x07\x00\x4B\x12\x00')#(b'\x08\xA6\x1D\x48\x0D\x00\x4B\x12\x00')#(b'\x08\x23\xB4\xEC\x11\x00\x4B\x12\x00')
+EndPoint = 0x0E
+ClusterOTA = 0x0019
+HaHeader = 0x00 #type = 01 direction = 1 no_rsp = 1
+
 def MyTxThread():
-	x = 1
-	circle = 0
-	while x < 50:
-		x = 1 + x
-		print('%d'% x)
-		time.sleep(0.5)
+	#print('第',i,'次读取')
+	while 1:
+		while s.inWaiting() == 0:
+			time.sleep(0.1)
+			pass
+		time.sleep(0.01)
+		n = s.inWaiting()
+		string = s.read(n)
+		#IEEE64 = string[7:16]
+		HexShow("\r    ReceiveBytes",string)# display the data received returning the last line.
+	#print(string)
+		
+def PackSendData( SendMsg, endpoint, cluster, headControl, Cmd):
+	SendMsg = struct.pack('<BBB', Send_SN, TyHeader, ProtocolType) + IEEE64 + struct.pack('<BHBB', endpoint, cluster, headControl, Cmd )+SendMsg
+	Send_Lenth = len(SendMsg) + 6 # 6 = start_2bytes + lenth_2bytes + crc16_2bytes
+	SendMsg =  struct.pack('<H',0xB55B) + struct.pack('<H',Send_Lenth) + SendMsg #add start lenth
+	SendMsg = SendMsg + struct.pack('<H', cal_crc16(SendMsg, len(SendMsg))) # add crc
+	#HexShow("tx", SendMsg)
+	return SendMsg		
 
 def HexShow(S_name,i_string):
 	hex_string = ''
@@ -18,24 +43,32 @@ def HexShow(S_name,i_string):
 		hex_string += hhex + ' '
 #	print('ReceiveBytes: %i_string' % (hex_string))
 	print(S_name,hex_string,'	total:',hLen)
-		
-Comnumb = 'com3'
+
+#crc16 calculate
+def cal_crc16(puchMsg,crc_count):
+	xorCRC = 0xA001
+	CRC = 0xFFFF
+	for i in range(crc_count):
+		CRC ^= puchMsg[i]
+		for j in range(8):
+			XORResult = CRC & 0x01
+			CRC >>= 1
+			if (XORResult & 0xFF):
+				CRC ^= xorCRC
+	return CRC
+###++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++###
+	
+Comnumb = 'com6'
 #Comnumb=input('输入串口号(如com9):')
 s = serial.Serial(Comnumb,115200)
 string = ' '
-s.setTimeout(0)
-#thrd = threading.Thread(target=MyTxThread,name='koing2010')
-#thrd.start()
+#s.setTimeout(0)
+thrd = threading.Thread(target=MyTxThread,name='koing2010')
+thrd.start()
 while(1):
-	#print('第',i,'次读取')
-	while s.inWaiting() == 0:
-		time.sleep(0.1)
-		pass
-	time.sleep(0.01)
-	n = s.inWaiting()
-	string = s.read(n)
-	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
-	HexShow("\r    ReceiveBytes",string)# display the data received returning the last line.
-	#print(string)
+	InputMsg = input('Input cluster:\n')
+	SendBytes = bytes.fromhex(InputMsg)
+	endpoint, cluster, headControl, Cmd = struct.unpack('<BHBB', SendBytes[0:5])
+	s.write( PackSendData(SendBytes[5:], endpoint, cluster, headControl, Cmd))
 s.close()
-#thrd.join()
+thrd.join()
--- serial_znp_server.py
+++ serial_znp_server.py
@@ -1,7 +1,36 @@
-import serial
+#import serial
 import time
 import threading
 import struct
+
+#The subsystem of the command
+CMD0_SUBSYSTEM_RPC_ERR_INTERFACE = 0
+CMD0_SUBSYSTEM_SYS_INTERFACE = 1
+CMD0_SUBSYSTEM_AF_INTERFACE = 4
+CMD0_SUBSYSTEM_ZDO_INTERFACE = 5
+CMD0_SUBSYSTEM_SIMPLE_API_INTERFACE = 6
+CMD0_SUBSYSTEM_UTIL_INTERFACE = 7
+CMD0_SUBSYSTEM_APP_INTERFACE = 9
+# The command type
+CMD0_TYPE_POLL = 0
+CMD0_TYPE_SREQ = 1
+CMD0_TYPE_AREQ = 2
+CMD0_TYPE_SRSP = 3
+
+sys_reset_req = (b'\x01\x41\x00\x00')
+sys_version = (b'\x00\x21\x02')
+sys_osal_nv_read_header = (b'\x03\x21\x08')
+
+#def
+def ProcessRxData( msg ):
+	tempMsg = calcFCS(msg[0,len(msg)])
+	print("%02X"%tempMsg)
+	if(tempMsg == msg[len(msg)-1]):
+		print()
+	else:
+		print("serial rx data fcs erro!")
+
+
 # XOR of all the bytes
 def calcFCS(pMsg):
 	result = 0
@@ -10,10 +39,11 @@ def calcFCS(pMsg):
 	return result & 0xFF
 
 #define the value of communication format
-def DataRequest(SendData,):
+def DataRequest(SendData):
 	# SOF  +  General format frame + FCS
 	#  1byte  + 3-253bytes  +  1bytes
 	SendData = struct.pack("<B", 0xFE) + SendData + struct.pack("<B",calcFCS(SendData))#XOR the general format frame fields
+	HexShow("txdata",SendData)
 	s.write(SendData)
 
 
@@ -35,23 +65,23 @@ def HexShow(S_name,i_string):
 #	print('ReceiveBytes: %i_string' % (hex_string))
 	print(S_name,hex_string,'	total:',hLen)
 		
-Comnumb = 'com14'
+Comnumb = 'com5'
 #Comnumb=input('输入串口号(如com9):')
-s = serial.Serial(Comnumb,115200)
-string = ' '
-s.setTimeout(0)
+#s = serial.Serial(Comnumb,115200)
+#s.setTimeout(0)
 #thrd = threading.Thread(target=MyTxThread,name='koing2010')
 #thrd.start()
-while(1):
-	#print('第',i,'次读取')
-	while s.inWaiting() == 0:
-		time.sleep(0.1)
-		pass
-	time.sleep(0.01)
-	n = s.inWaiting()
-	string = s.read(n)
-	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
-	HexShow("\r    ReceiveBytes",string)# display the data received returning the last line.
+#while(1):
+#	#print('第',i,'次读取')
+#	while s.inWaiting() == 0:
+#		time.sleep(0.1)
+#		pass
+#	time.sleep(0.01)
+#	n = s.inWaiting()
+#	string = s.read(n)
+#	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
+#	HexShow("\r    ReceiveBytes",string)# display the data received returning the last line.
 	#print(string)
-s.close()
+DataRequest(sys_osal_nv_read_header + struct.pack("<HB", 0x0F01, 0x00))
+#s.close()
 #thrd.join()
