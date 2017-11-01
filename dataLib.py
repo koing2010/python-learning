a8bdfbe23231ff1886e311db104ddd7d8a57492b
koing2010 <koing2010@163.com>
2017-11-01 19:48:54 +0800

    sql 表查改功能

--- dataLib.py
+++ dataLib.py
@@ -3,35 +3,40 @@ import _sqlite3
 #cnnect the server of sqlite
 Map = _sqlite3.connect('Mac2ShortMap.db')
 
-#creat a operation cursor
-MapCursor = Map.cursor()
-newdev = 'F7 7F BB 0D 00 4B 12 00'
-MapCursor.execute('select * from user where MacAddr=?', (newdev,))
+def OpenMap():
+    Map = _sqlite3.connect('Mac2ShortMap.db')
 
-value = MapCursor.fetchall()
-
-if value == []:
-    print("No This Device!\n")
-else:
-    print("Cread New Device Map\n")
-    print(value)
-
-#creat a data table by the SQL CMD
-#MapCursor.execute('create table user (MacAddr varchar(23) primary key, ShortAddr varchar(4))')
-
-#MapCursor.execute('insert into user (MacAddr, ShortAddr) values (\'F7 7F BB 0D 00 4B 12 00\', \'5680\')')
-
-print("SQL Insert Line: %d\n"%MapCursor.rowcount)
-
-#close the Cursor
-MapCursor.close()
-
-
-
-
-Map.commit()
-
-Map.close()
+# return the short address
 def GetShortAddress(Mac):
+    # creat a operation cursor
+    MapCursor = Map.cursor()
+    MapCursor.execute('select * from user where MacAddr=?', (Mac,))
+    value = MapCursor.fetchall()
+    MapCursor.close()
+    return value;
 
-    return 0;
+#if map is existing already, it will modify the map. if not, it will add a table
+def InsetMap(Mac, SortAddr):
+    MapCursor = Map.cursor()
+    MapCursor.execute('select * from user where MacAddr=?', (Mac,))
+    value = MapCursor.fetchall()
+    if value  == []:
+        print("No This Device!\n")
+        MapCursor.execute('insert into user (MacAddr, ShortAddr) values (Mac, SortAddr)')
+    else:
+        print("Cread New Device Map\n")
+        print(value)
+        MapCursor.execute("update user set ShortAddr=? where MacAddr=?", (SortAddr,Mac))#('update user set ShortAddr =?,where MacAddr=?', (SortAddr,Mac))
+    print("SQL Insert Line: %d\n" % MapCursor.rowcount)
+    Map.commit()
+    MapCursor.close()
+
+
+def CloseMap():
+    Map.close()
+
+def main():
+    InsetMap("F7 7F BB 0D 00 4B 12 00", "2345")
+    CloseMap()
+if __name__ == '__main__':
+    main()
