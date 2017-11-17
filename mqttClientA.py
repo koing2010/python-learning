1dc5a7b598b5c1a2d0b46e31db06af896ccd410e
koing2010 <koing2010@163.com>
2017-11-17 20:04:26 +0800

    mqtt 测试通过 可以成功连接server, 远程拒绝连接时必须设置client_id

---
+++ mqttClientA.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+Topic = "old"
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe( Topic )
+
+def on_message(client, userdata, msg):
+    print("topic: "+msg.topic+" Msg: "+str(msg.payload))
+    #print( client)
+    print("Enter Msg")
+
+client = mqtt.Client(client_id="mac_id1234567891")
+client.username_pw_set("admin", "1111111")
+client.on_connect = on_connect
+client.on_message = on_message
+
+HOST = "47.94.155.129"
+
+client.connect(HOST, 61613, 60)
+#client.loop_forever()
+#user = input("Enter Your Name:")
+#client.user_data_set(user)
+client.loop_start()
+
+while True:
+    Msg = input()
+    if Msg:
+        client.publish(Topic, Msg)
